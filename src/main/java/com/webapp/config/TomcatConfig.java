3056d92316e67e569f0f876936eec14931396796
Whytanishq <tanishqsaraf2005@gmail.com>
2025-05-22 17:20:14 +0530

    initial commit

---
+++ src/main/java/com/webapp/config/TomcatConfig.java
@@ -0,0 +1,15 @@
+package com.webapp.config;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TomcatConfig implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
+    @Override
+    public void customize(TomcatServletWebServerFactory factory) {
+        factory.addContextCustomizers(context -> {
+            context.setAddWebinfClassesResources(true);
+        });
+}
+}
---
+++ src/main/java/com/webapp/respository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.webapp.respository;
+
+import com.webapp.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
